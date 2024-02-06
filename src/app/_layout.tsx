c84770e3db7b98291936126e7e6bc5f24054d33f
gustavogss <gustavogss.dev@gmail.com>
2024-02-06 20:09:47 -0300

    feat: fonts configuration

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,30 @@
 import { Slot } from 'expo-router';
 import React from 'react';
 import { SafeAreaView } from 'react-native';
+import {
+    useFonts,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+} from '@expo-google-fonts/inter';
+import { Loading } from '../components/loading';
 
 
 export default function Layout() {
-  return (
-    <SafeAreaView className='bg-slate-900 flex-1 '>
-        <Slot />
-    </SafeAreaView>
-  );
+    const [fonstLoad] = useFonts({
+        Inter_400Regular,
+        Inter_500Medium,
+        Inter_600SemiBold,
+        Inter_700Bold,
+    });
+
+    if (!fonstLoad) {
+        return <Loading />
+    }
+    return (
+        <SafeAreaView className='bg-slate-900 flex-1 '>
+            <Slot />
+        </SafeAreaView>
+    );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+
 export default function Home() {
     return (
         <View className='flex-1 pt-8'>
